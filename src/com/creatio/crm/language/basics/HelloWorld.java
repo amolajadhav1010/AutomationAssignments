7219876b792792485cb75b26bc0fc74b578a2f26
Amol <amol1010.jadhav@gmail.com>
2024-12-20 23:44:39 +0530

    Assignment 1 - Data Types

---
+++ src/com/creatio/crm/language/basics/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.creatio.crm.language.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.write(65);
+		System.out.print(" ");
+			
+	}
+
+}

1995edb569b354a7d2bc8d048825d4c178a8066c
Amol <amol1010.jadhav@gmail.com>
2024-12-23 20:21:44 +0530

    Assignment 2 - Arrays

--- src/com/creatio/crm/language/basics/Assignment2_Arrays.java
+++ src/com/creatio/crm/language/basics/Assignment2_Arrays.java
@@ -1,17 +1,45 @@
 package com.creatio.crm.language.basics;
 
-public class Assignment2_Arrays 
+public class Assignment2_Arrays
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		String Category1Groceries [][][][] = new String [2][5][4][5];
-				
-		Category1Groceries [0][0][0][0] = "Groceries";
-		Category1Groceries [0][0][0][0] = "1";
-		Category1Groceries [0][0][0][0] = "Beverages";
-		Category1Groceries [0][0][0][0] = "Cola";
-
-	}
+		//System.out.println("Product ID\t Category\t Product Name");
+		String [][][][] Groceries = new String[2][5][4][5];
+		Groceries [0][0][0][0] = "Groceries";
+		Groceries [0][0][0][0] = "1";
+		Groceries [0][0][0][0] = "Beverage";
+		Groceries [0][0][0][0] = "Cola";
+		
+		Groceries [0][0][0][0] = "Groceries";
+		Groceries [0][1][0][0] = "2";
+		Groceries [0][0][0][0] = "Beverage";
+		Groceries [0][0][0][1] = "Orange Juice";
+		
+		Groceries [0][0][0][0] = "Groceries";
+		Groceries [0][2][0][0] = "3";
+		Groceries [0][0][0][0] = "Beverage";
+		Groceries [0][0][0][2] = "Lemonade";
+		
+		Groceries [0][0][0][0] = "Groceries";
+		Groceries [0][3][0][0] = "4";
+		Groceries [0][0][0][0] = "Beverage";
+		Groceries [0][0][0][3] = "Green Tea";
+		
+		Groceries [0][0][0][0] = "Groceries";
+		Groceries [0][4][0][0] = "5";
+		Groceries [0][0][0][0] = "Beverage";
+		Groceries [0][0][0][4] = "Green Tea";
+		
+		
+		Groceries [0][0][0][0] = "Groceries";
+		Groceries [0][0][1][0] = "1";
+		Groceries [0][0][0][0] = "Snacks";
+		Groceries [0][0][0][0] = "Pop Corn";
+		
+		
+		
+			}
 
 }
